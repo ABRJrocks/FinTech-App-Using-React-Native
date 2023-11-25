2ea0eb6d03da4fd6b509c253ce2bb245516df802
Abdul Rafay <40339959+ABRJrocks@users.noreply.github.com>
2023-11-26 04:46:00 +0500

    TopUp screen complete

--- src/screens/TopUp.jsx
+++ src/screens/TopUp.jsx
@@ -12,57 +12,10 @@ import {
 import { StatusBar } from "expo-status-bar";
 
 const TopUp = (props) => {
-  const [searchText, setSearchText] = useState("");
-  const [recentItems, setRecentItems] = useState([
-    { id: "1", name: "John Doe", icon: require("../../assets/contact.png") },
-    { id: "2", name: "Jane Smith", icon: require("../../assets/contact.png") },
-    { id: "3", name: "Bob Johnson", icon: require("../../assets/contact.png") },
-    {
-      id: "4",
-      name: "Alice Williams",
-      icon: require("../../assets/contact.png"),
-    },
-    { id: "5", name: "David Brown", icon: require("../../assets/contact.png") },
-    { id: "6", name: "Sarah Davis", icon: require("../../assets/contact.png") },
-    { id: "7", name: "Michael Lee", icon: require("../../assets/contact.png") },
-    {
-      id: "8",
-      name: "Emily Wilson",
-      icon: require("../../assets/contact.png"),
-    },
-    { id: "9", name: "Robert Chen", icon: require("../../assets/contact.png") },
-    {
-      id: "10",
-      name: "Olivia Davis",
-      icon: require("../../assets/contact.png"),
-    },
-    {
-      id: "11",
-      name: "William Brown",
-      icon: require("../../assets/contact.png"),
-    },
-    {
-      id: "12",
-      name: "Sophia Wilson",
-      icon: require("../../assets/contact.png"),
-    },
-    {
-      id: "13",
-      name: "James Johnson",
-      icon: require("../../assets/contact.png"),
-    },
-    {
-      id: "14",
-      name: "Emma Williams",
-      icon: require("../../assets/image.png"),
-    },
-    { id: "15", name: "Daniel Lee", icon: require("../../assets/image.png") },
-  ]);
-
-  const filteredRecentItems = recentItems.filter((item) =>
-    item.name.toLowerCase().includes(searchText.toLowerCase())
-  );
-
+  const [selectedAmount, setSelectedAmount] = useState("10 $");
+  const handleAmountSelection = (amount) => {
+    setSelectedAmount(amount);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -72,32 +25,95 @@ const TopUp = (props) => {
             source={require("../../assets/x.png")}
           />
         </TouchableOpacity>
-        <Text style={styles.headerTitle}>Recent</Text>
         <Image
           style={{ width: 22, height: 22 }}
           source={require("../../assets/help.png")}
         />
       </View>
-      <TextInput
-        placeholderTextColor="white"
-        style={styles.searchBar}
-        placeholder="Search..."
-        value={searchText}
-        onChangeText={(text) => setSearchText(text)}
-      />
-      <FlatList
-        data={searchText ? filteredRecentItems : recentItems}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.recentItem}>
+      <View>
+        <Text style={styles.headerTitle}>Auto top-up</Text>
+        <Text style={styles.subTitle}>
+          Your account will be top-up through your bank card.
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.ammountText}>
+          When the balance is below{" "}
+          <Text style={{ fontWeight: "bold" }}>{selectedAmount}</Text>
+        </Text>
+        <View style={styles.optionsContainer}>
+          <TouchableOpacity
+            style={[
+              styles.option,
+              selectedAmount === "10 $" && styles.selectedOption,
+            ]}
+            onPress={() => handleAmountSelection("10 $")}
+          >
+            <Text style={styles.optionText}>10 $</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.option,
+              selectedAmount === "20 $" && styles.selectedOption,
+            ]}
+            onPress={() => handleAmountSelection("20 $")}
+          >
+            <Text style={styles.optionText}>20 $</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.option,
+              selectedAmount === "50 $" && styles.selectedOption,
+            ]}
+            onPress={() => handleAmountSelection("50 $")}
+          >
+            <Text style={styles.optionText}>50 $</Text>
+          </TouchableOpacity>
+        </View>
+        {/* Select Bank */}
+        <View style={styles.bankContainer}>
+          <Text
+            style={{
+              color: "white",
+              marginLeft: 20,
+              marginTop: 20,
+              fontSize: 20,
+            }}
+          >
+            Using:
+          </Text>
+          <TouchableOpacity>
+            <View style={styles.bankChoice}>
+              <Image
+                style={{ width: 26, height: 26 }}
+                source={require("../../assets/bank.png")}
+              />
+              <Text style={styles.bankName}>Danske Bank</Text>
+              <Image
+                style={{
+                  width: 20,
+                  height: 20,
+                  position: "absolute",
+                  right: 0,
+                  top: 10,
+                  resizeMode: "contain",
+                }}
+                source={require("../../assets/arrow.png")}
+              />
+            </View>
+          </TouchableOpacity>
+          <View style={styles.feeContainer}>
             <Image
-              source={item.icon}
-              style={{ width: 30, height: 30, marginRight: 15 }}
+              style={{ width: 22, height: 22 }}
+              source={require("../../assets/info.png")}
             />
-            <Text style={{ color: "white", fontSize: 16 }}>{item.name}</Text>
+            <Text style={styles.feeText}>Fee: 0.10 $</Text>
           </View>
-        )}
-      />
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Confirm</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -117,28 +133,93 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     color: "white",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  searchBar: {
-    borderBottomWidth: 1,
     fontSize: 30,
-    borderBottomColor: "#3B3D38",
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 30,
-    paddingHorizontal: 20,
-    color: "#999999",
+    fontWeight: "bold",
+    marginLeft: 20,
+    marginTop: 20,
   },
-  recentItem: {
+  subTitle: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    marginLeft: 20,
+    marginTop: 20,
+    paddingRight: 20,
+    width: 320,
+  },
+  ammountText: {
+    color: "white",
+    fontSize: 20,
+    marginLeft: 20,
+    marginTop: 50,
+    paddingRight: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#A3A3A3",
+    marginRight: 20,
+    paddingBottom: 5,
+  },
+  optionsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 60,
+  },
+  option: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+  },
+  optionText: {
+    color: "white",
+    fontSize: 20,
+  },
+  selectedOption: {
+    borderBottomColor: "#5BB1E0",
+    borderBottomWidth: 2,
+  },
+  bankContainer: {
+    marginTop: 60,
+  },
+  bankChoice: {
     flexDirection: "row",
     alignItems: "center",
-    padding: 10,
+    marginTop: 30,
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    borderBottomColor: "#A3A3A3",
     borderBottomWidth: 1,
-    borderBottomColor: "#3B3D38",
-    marginBottom: 10,
-    paddingHorizontal: 20,
-    borderColor: "#999999",
+    paddingBottom: 10,
+  },
+  bankName: {
+    color: "white",
+    fontSize: 24,
+    marginLeft: 25,
+  },
+  feeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 20,
+  },
+  feeText: {
+    color: "white",
+    fontSize: 12,
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: "#5BB1E0",
+    borderRadius: 10,
+    position: "absolute",
+    bottom: 40,
+    left: 20,
+    right: 20,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
   },
 });
 
